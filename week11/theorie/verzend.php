d986bddd11b2d9d1deda54043ea776df3c6f93bd
gdm-1718-gillvijn <32244394+gdm-1718-gillvijn@users.noreply.github.com>
2018-05-07 13:04:03 +0200

    webtech week 11

---
+++ week11/theorie/verzend.php
@@ -0,0 +1,24 @@
+<?php
+//superglobals
+ $username = $_GET['username'];
+ $favdate = $_GET['fav-date'];
+ if(isset($*_POST['fav-date'])) && !empty($_POST['fav-date'])) //isset wordt gebruikt om te kijken of je op een knop hebt geklikt of niet
+ {
+     $favdate = $_POST['fav-date'];
+ }
+// echo '<h1>' . $username . '</h1>' ;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?=$username ?></h1>
+    <p>Je favoriete weekdag is : <?=$favdate ?> </p>
+</body>
+</html>
